23a78e244a71d849def4e73a9bd34c1d2cda215a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-27 13:06:33 +0330

    Update tcs

--- dist/002-functions.js
+++ dist/002-functions.js
@@ -1,15 +1,6 @@
 "use strict";
-// Variables
-Object.defineProperty(exports, "__esModule", { value: true });
-let name = "Amirhossein"; // var company is always string
-let age = 24; // var age is always number
-let isMale = true; // var isPublished is always boolean
-let odds = [1, 3, 5, 7, 9]; // Array of numbers
-let evens = [2, 4, 6, 8]; // Array of numbers
-let friends = ["Reza", "Sina", "Kasra"]; // Array of strings
-let x = "Amirhossein"; // This var can be anything
-let xArr = ["Amir", 24, true, null, [], {}]; // This array can have anything
 // Functions
+Object.defineProperty(exports, "__esModule", { value: true });
 const concatStrings = (a, b) => {
     // This function get 2 variables that both are string
     // It returns always string
---
+++ dist/003-interfaces.js
@@ -0,0 +1,21 @@
+"use strict";
+// Interfaces
+Object.defineProperty(exports, "__esModule", { value: true });
+// A person with out Spouse
+const Amirhossein = {
+    name: "Amirhossein",
+    age: 24,
+    isMale: true,
+    friends: ["Sina", "Kasra"],
+};
+// A person with Spouse
+const Beyonce = {
+    name: "Beyonce",
+    age: 44,
+    isMale: false,
+    friends: ["Rihanna", "Adele", "Shakira"],
+    spouse: "Jay Z",
+};
+console.log(Amirhossein.spouse);
+console.log(Beyonce.spouse);
+//# sourceMappingURL=003-interfaces.js.map
