5c158b1b6193d3aaa6a83eba1430d6d481a54cc0
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-27 13:14:55 +0330

    Nice. We can procced

--- 003-interfaces.ts
+++ 003-interfaces.ts
@@ -8,6 +8,8 @@ interface PersonInterface {
   isMale: boolean; // isMake is required and boolean
   friends: string[]; // Friends is required and array of strings
   spouse?: string; // Spouse is optional but string
+  sayHi(message: string): void; // A function that gets string and returns a void
+  canTalk: boolean;
 }
 
 // A person with out Spouse
@@ -16,6 +18,11 @@ const Amirhossein: PersonInterface = {
   age: 24,
   isMale: true,
   friends: ["Sina", "Kasra"],
+
+  sayHi(message: string): void {
+    console.log(message);
+  },
+  canTalk: false,
 };
 
 // A person with Spouse
@@ -25,7 +32,19 @@ const Beyonce: PersonInterface = {
   isMale: false,
   friends: ["Rihanna", "Adele", "Shakira"],
   spouse: "Jay Z",
+  sayHi(message: string): void {
+    console.log(message);
+  },
+  canTalk: true,
 };
 
 console.log(Amirhossein.spouse);
-console.log(Beyonce.spouse)
+console.log(Beyonce.spouse);
+
+if (Beyonce.canTalk) {
+  Beyonce.sayHi("Halo!!!!!");
+}
+
+if (Amirhossein.canTalk) {
+  Beyonce.sayHi("Amirhossein can talk like Beyonce!");
+}
